458cce4f15a6d3631734eff150c5c7408109c54b
tlans21 <cjstlans21@naver.com>
2023-03-31 15:24:05 +0900

    store

---
+++ main.js
@@ -0,0 +1,8 @@
+const port = 3000,
+    http = require('http'),
+    httpStatus = require('http-status-codes'),
+    router = require('./router'),
+    contentTypes = require('./contetn-types'),
+    utils = require('./utils');
+
+http.createServer();
